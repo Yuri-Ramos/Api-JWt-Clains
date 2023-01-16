fc1242d57e534f7b7bcf635beb42a122906394cc
Yuri Ramos <yroramos@gmail.com>
2023-01-16 15:14:52 -0300

    primeiro end point com map

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Api_JWt_Clains.Data;
+using Microsoft.EntityFrameworkCore;
+using Api_JWt_Clains.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MinimalContextDb>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 var app = builder.Build();
 
@@ -21,5 +28,25 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/fonercedor",async (MinimalContextDb context)=> await context.Fornecedores.ToListAsync())
+.WithName("GetFornecedor")
+.WithTags("Fornecedor");
+
+app.MapGet("/fonercedor/{id}",async 
+(Guid id,
+ MinimalContextDb context)=> 
+       await context.Fornecedores.FindAsync(id)
+              is Fornecedor fornecedor
+                  ? Results.Ok(fornecedor)
+                  : Results.NotFound())        
+        .Produces<Fornecedor>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound)
+        .WithName("GetFornecedorPorId")
+        .WithTags("Fornecedor");
+
+app.MapPost("/Fornecedor", async (
+   )=>
+)
 
 app.Run();
