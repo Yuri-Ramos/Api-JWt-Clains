6052775510aa0ede6c2b749d27ec61e8c631d104
Yuri Ramos <yroramos@gmail.com>
2023-01-17 11:03:10 -0300

    Crud finalizado :zap:

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Api_JWt_Clains.Data;
 using Microsoft.EntityFrameworkCore;
 using Api_JWt_Clains.Models;
 using MiniValidation;
+using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,42 +31,81 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.MapGet("/fonercedor",async (MinimalContextDb context)=> await context.Fornecedores.ToListAsync())
+app.MapGet("/fonercedor", async (MinimalContextDb context) => await context.Fornecedores.ToListAsync())
 .WithName("GetFornecedor")
 .WithTags("Fornecedor");
 
-app.MapGet("/fonercedor/{id}",async 
+app.MapGet("/fonercedor/{id}", async
 (Guid id,
- MinimalContextDb context)=> 
+ MinimalContextDb context) =>
        await context.Fornecedores.FindAsync(id)
               is Fornecedor fornecedor
                   ? Results.Ok(fornecedor)
-                  : Results.NotFound())        
+                  : Results.NotFound())
         .Produces<Fornecedor>(StatusCodes.Status200OK)
         .Produces(StatusCodes.Status404NotFound)
         .WithName("GetFornecedorPorId")
         .WithTags("Fornecedor");
 
 
-    app.MapPost("/fornecedor", async (
-        MinimalContextDb context,
-        Fornecedor fornecedor) =>
-    {
-        if (!MiniValidator.TryValidate(fornecedor, out var errors))
-            return Results.ValidationProblem(errors);
+app.MapPost("/fornecedor", async (
+  MinimalContextDb context,
+  Fornecedor fornecedor) =>
+  {
+      context.Fornecedores.Add(fornecedor);
+      var result = await context.SaveChangesAsync();
+  })
+  .Produces<Fornecedor>(StatusCodes.Status201Created)
+  .Produces(StatusCodes.Status400BadRequest)
+  .WithName("PostFornecedor")
+  .WithTags("Fornecedor");
 
-        context.Fornecedores.Add(fornecedor);
-        var result = await context.SaveChangesAsync();
+app.MapPut("/fornecedor/{id}", async (
+    Guid id, MinimalContextDb context, Fornecedor fornecedor
 
-        return result > 0
-            //? Results.Created($"/fornecedor/{fornecedor.Id}", fornecedor)
-            ? Results.CreatedAtRoute("GetFornecedorPorId", new { id = fornecedor.Id }, fornecedor)
-            : Results.BadRequest("Houve um problema ao salvar o registro");
+) =>
+{
+    var fornecedorBanco = await context.Fornecedores.FindAsync(id);
+    if (fornecedorBanco == null) return Results.NotFound();
+ if (!MiniValidator.TryValidate(fornecedor, out var errors))
+        return Results.ValidationProblem(errors);
+    context.Fornecedores.Update(fornecedor);
+    var result = await context.SaveChangesAsync();
 
-    }).ProducesValidationProblem()
-    .Produces<Fornecedor>(StatusCodes.Status201Created)
-    .Produces(StatusCodes.Status400BadRequest)
-    .WithName("PostFornecedor")
-    .WithTags("Fornecedor");
+    return result > 0
+        ? Results.NoContent()
+        : Results.BadRequest("Houve um problema ao salvar o registro");
+
+
+}).ProducesValidationProblem()
+.Produces(StatusCodes.Status204NoContent)
+.Produces(StatusCodes.Status400BadRequest)
+.WithName("PutFornecedor")
+.WithTags("Fornecedor");
+
+
+
+
+app.MapDelete("/fornecedor/{id}", async (
+    Guid id,
+    MinimalContextDb context) =>
+{
+    var fornecedor = await context.Fornecedores.FindAsync(id);
+    if (fornecedor == null) return Results.NotFound();
+
+    context.Fornecedores.Remove(fornecedor);
+    var result = await context.SaveChangesAsync();
+
+    return result > 0
+        ? Results.NoContent()
+        : Results.BadRequest("Houve um problema ao salvar o registro");
+
+}).Produces(StatusCodes.Status400BadRequest)
+.Produces(StatusCodes.Status204NoContent)
+.Produces(StatusCodes.Status404NotFound)
+.RequireAuthorization("ExcluirFornecedor")
+.WithName("DeleteFornecedor")
+.WithTags("Fornecedor");
+
 
 app.Run();
